99ae7291de0605691fd80b6c6fac92f5e6bea689
ChHarding <charding@iastate.edu>
2022-06-01 14:08:14 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,1 @@
-#Hello this is my First Github pull request 
-#It is pretty cool to learn how to use Github!!
-#Happy Day!
+print("Hello this is my First Github pull request. It is pretty cool to learn how to use Github!! Happy Day!")
